fdf959b71d0e383b2559730e16f776cac6734ebb
Devon Blumenthal <dmblumen@gmail.com>
2018-11-15 10:22:18 -0500

    "Extra Stuff"

--- DBML.R
+++ DBML.R
@@ -4,8 +4,23 @@ library(tidyverse)
 library(ggplot2)
 library(randomForest)
 library(caret)
-
-samps = createDataPartition(df$SalePrice,p = .8,list = F)
-trn = df[samps,]
-tst = df[-samps,]
+library(Metrics)
+set.seed(0)
+dfN = df[,sapply(df,is.numeric) | sapply(df,is.integer)]
+dfC = df[,sapply(df,is.character) | sapply(df,is.factor)]
+dfC = model.matrix(~ .,data = dfC)[,-1]
+dfC = as.data.frame(dfC)
+DF = cbind(dfN,dfC)
+DF$SalePrice = log(DF$SalePrice)
+samps = createDataPartition(DF$SalePrice,p = .8,list = F)
+trn = DF[samps,]
+tst = DF[-samps,]
+mtries = sqrt(nrow(trn))
 cont  = trainControl(method = "cv",number = 10)
+tune = expand.grid(mtry = mtries)
+rforest = train(SalePrice ~ .,data = trn,trControl = cont,tuneGrid = tune)
+preds = predict(rforest,newdata = tst,na.action = na.omit)
+str(df)
+rmsle(exp(tst$SalePrice),exp(preds))
+
+     
